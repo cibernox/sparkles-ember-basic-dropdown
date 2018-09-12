1f2a9e98a1146f073b63f73c960a510f3641666f
Miguel Camba <miguel.camba@gmail.com>
2018-09-13 01:01:27 +0200

    Implement most content tests

--- tests/integration/components/basic-dropdown-content-test.js
+++ tests/integration/components/basic-dropdown-content-test.js
@@ -25,281 +25,150 @@ module('Integration | Component | basic-dropdown-content', function (hooks) {
     assert.dom('#destination-el .ember-basic-dropdown-content').doesNotExist('It isn\'t rendered in the #ember-testing div');
   });
 
-  // test('It derives the ID of the content from the `uniqueId` property of of the dropdown', async function (assert) {
-  //   assert.expect(1);
-  //   this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el'}}Lorem ipsum{{/basic-dropdown/content}}
-  //   `);
-  //   assert.dom('.ember-basic-dropdown-content').hasAttribute('id', 'ember-basic-dropdown-content-e123', 'contains the expected ID');
-  // });
+  test('It derives the ID of the content from the `uniqueId` property of of the dropdown', async function (assert) {
+    assert.expect(1);
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdown @initiallyOpened={{true}} @destination="destination-el" as |dd|>
+        <dd.Trigger>Click me</dd.Trigger>
+        <dd.Content>Content</dd.Content>
+      </BasicDropdown>
+    `);
+    assert.dom('.ember-basic-dropdown-content').hasAttribute('id', /ember-basic-dropdown-content-ember\d+/, 'contains the expected ID');
+  });
 
-  // test('If it receives `class="foo123"`, the rendered content will have that class along with the default one', async function (assert) {
-  //   assert.expect(1);
-  //   this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' class="foo123"}}Lorem ipsum{{/basic-dropdown/content}}
-  //   `);
-  //   assert.dom('.ember-basic-dropdown-content').hasClass('foo123', 'The dropdown contains that class');
-  // });
+  test('If it receives `class="foo123"`, the rendered content will have that class along with the default one', async function (assert) {
+    assert.expect(1);
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdown @initiallyOpened={{true}} @destination="destination-el" as |dd|>
+        <dd.Trigger>Click me</dd.Trigger>
+        <dd.Content class="foo123">Content</dd.Content>
+      </BasicDropdown>
+    `);
+    assert.dom('.ember-basic-dropdown-content').hasClass('foo123', 'The dropdown contains that class');
+  });
 
-  // test('If it receives `defaultClass="foo123"`, the rendered content will have that class along with the default one', async function (assert) {
-  //   assert.expect(1);
-  //   this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' defaultClass="foo123"}}Lorem ipsum{{/basic-dropdown/content}}
-  //   `);
-  //   assert.dom('.ember-basic-dropdown-content').hasClass('foo123', 'The dropdown contains that class');
-  // });
+  test('If it receives `dir="rtl"`, the rendered content will have the attribute set', async function (assert) {
+    assert.expect(1);
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdown @initiallyOpened={{true}} @destination="destination-el" as |dd|>
+        <dd.Trigger>Click me</dd.Trigger>
+        <dd.Content dir="rtl">Content</dd.Content>
+      </BasicDropdown>
+    `);
+    assert.dom('.ember-basic-dropdown-content').hasAttribute('dir', 'rtl', 'The dropdown has `dir="rtl"`');
+  });
 
-  // test('If it receives `dir="rtl"`, the rendered content will have the attribute set', async function (assert) {
-  //   assert.expect(1);
-  //   this.dropdown = { isOpen: true, actions: { reposition() { } } };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' dir="rtl"}}Lorem ipsum{{/basic-dropdown/content}}
-  //   `);
-  //   assert.dom('.ember-basic-dropdown-content').hasAttribute('dir', 'rtl', 'The dropdown has `dir="rtl"`');
-  // });
+  // Focus
+  test('If it receives an `onFocusIn` action, it is invoked if a focusin event is fired inside the content', async function (assert) {
+    assert.expect(3);
+    this.onFocusIn = (dropdown, e) => {
+      assert.ok(true, 'The action is invoked');
+      assert.ok(dropdown.hasOwnProperty("uniqueId"), "receives the dropdown as 1st argument");
+      assert.ok(e instanceof window.Event, 'the second argument is an event');
+    };
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdown @initiallyOpened={{true}} @destination="destination-el" as |dd|>
+        <dd.Trigger>Click me</dd.Trigger>
+        <dd.Content @onFocusIn={{this.onFocusIn}}><input type="text" id="test-input-focusin" /></dd.Content>
+      </BasicDropdown>
+    `);
+    await focus('#test-input-focusin');
+  });
 
-  // // Clicking while the component is opened
-  // test('Clicking anywhere in the app outside the component will invoke the close action on the dropdown', async function (assert) {
-  //   assert.expect(1);
-  //   this.dropdown = {
-  //     uniqueId: 'e123',
-  //     isOpen: true,
-  //     actions: {
-  //       close() {
-  //         assert.ok(true, 'The close action gets called');
-  //       },
-  //       reposition() { }
-  //     }
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     <div id="other-div"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el'}}Lorem ipsum{{/basic-dropdown/content}}
-  //   `);
+  test('If it receives an `onFocusOut` action, it is invoked if a focusout event is fired inside the content', async function (assert) {
+    assert.expect(3);
+    this.onFocusOut = (dropdown, e) => {
+      assert.ok(true, 'The action is invoked');
+      assert.ok(dropdown.hasOwnProperty("uniqueId"), "receives the dropdown as 1st argument");
+      assert.ok(e instanceof window.Event, 'the second argument is an event');
+    };
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdown @initiallyOpened={{true}} @destination="destination-el" as |dd|>
+        <dd.Trigger>Click me</dd.Trigger>
+        <dd.Content @onFocusOut={{this.onFocusOut}}><input type="text" id="test-input-focusin" /></dd.Content>
+      </BasicDropdown>
+    `);
+    await focus('#test-input-focusin');
+    await blur('#test-input-focusin');
+  });
 
-  //   await click('#other-div');
-  // });
+  // Mouseenter/leave
+  test('If it receives an `onMouseEnter` action, it is invoked if a mouseenter event is fired on the content', async function (assert) {
+    assert.expect(3);
+    this.onMouseEnter = (dropdown, e) => {
+      assert.ok(true, 'The action is invoked');
+      assert.ok(dropdown.hasOwnProperty("uniqueId"), "receives the dropdown as 1st argument");
+      assert.ok(e instanceof window.Event, 'the second argument is an event');
+    };
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdown @initiallyOpened={{true}} @destination="destination-el" as |dd|>
+        <dd.Trigger>Click me</dd.Trigger>
+        <dd.Content @onMouseEnter={{this.onMouseEnter}}>Content</dd.Content>
+      </BasicDropdown>
+    `);
+    await triggerEvent('.ember-basic-dropdown-content', 'mouseenter');
+  });
 
-  // test('Clicking anywhere inside the dropdown content doesn\'t invoke the close action', async function (assert) {
-  //   assert.expect(0);
-  //   this.dropdown = {
-  //     uniqueId: 'e123',
-  //     isOpen: true,
-  //     actions: {
-  //       close() {
-  //         assert.ok(false, 'The close action should not be called');
-  //       },
-  //       reposition() { }
-  //     }
-  //   };
+  test('If it receives an `onMouseLeave` action, it is invoked if a mouseleave event is fired on the content', async function (assert) {
+    assert.expect(3);
+    this.onMouseLeave = (dropdown, e) => {
+      assert.ok(true, "The action is invoked");
+      assert.ok(dropdown.hasOwnProperty("uniqueId"), "receives the dropdown as 1st argument");
+      assert.ok(e instanceof window.Event, "the second argument is an event");
+    };
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdown @initiallyOpened={{true}} @destination="destination-el" as |dd|>
+        <dd.Trigger>Click me</dd.Trigger>
+        <dd.Content @onMouseLeave={{this.onMouseLeave}}>Content></dd.Content>
+      </BasicDropdown>
+    `);
+    await triggerEvent('.ember-basic-dropdown-content', 'mouseleave');
+  });
 
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el'}}<div id="inside-div">Lorem ipsum</div>{{/basic-dropdown/content}}
-  //   `);
-  //   await click('#inside-div');
-  // });
+  // Keydown
+  test('If it receives an `onKeyDown` action, it is invoked if a keydown event is fired on the content', async function (assert) {
+    assert.expect(3);
+    this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
+    this.onKeyDown = (dropdown, e) => {
+      assert.ok(e instanceof window.Event, "It receives the event as second argument");
+      assert.ok(dropdown.hasOwnProperty("uniqueId"), "receives the dropdown as 1st argument");
+      assert.equal(e.keyCode, 70, "the event is the keydown event");
+    };
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdown @initiallyOpened={{true}} @destination="destination-el" as |dd|>
+        <dd.Trigger>Click me</dd.Trigger>
+        <dd.Content @onKeyDown={{this.onKeyDown}}><input type="text" id="inner-input" /></dd.Content>
+      </BasicDropdown>
+    `);
+    await focus('#inner-input');
+    await triggerKeyEvent('#inner-input', 'keydown', 70);
+  });
 
-  // test('Clicking in inside the a dropdown content nested inside another dropdown content doesn\'t invoke the close action on neither of them if the second is rendered in place', async function (assert) {
-  //   assert.expect(0);
-  //   this.dropdown1 = {
-  //     uniqueId: 'ember1',
-  //     isOpen: true,
-  //     actions: {
-  //       close() {
-  //         assert.ok(false, 'The close action should not be called');
-  //       },
-  //       reposition() {
-  //         return {};
-  //       }
-  //     }
-  //   };
-  //   this.dropdown2 = {
-  //     uniqueId: 'ember2',
-  //     isOpen: true,
-  //     actions: {
-  //       close() {
-  //         assert.ok(false, 'The close action should not be called either');
-  //       },
-  //       reposition() {
-  //         return {};
-  //       }
-  //     }
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     <div id="fake-trigger"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown1 destination='destination-el'}}
-  //       Lorem ipsum
-  //       {{#basic-dropdown/content dropdown=dropdown2 destination='destination-el' renderInPlace=true}}
-  //         <div id="nested-content-div">dolor sit amet</div>
-  //       {{/basic-dropdown/content}}
-  //     {{/basic-dropdown/content}}
-  //   `);
-
-  //   await click('#nested-content-div');
-  // });
-
-  // // Touch gestures while the component is opened
-  // test('Tapping anywhere in the app outside the component will invoke the close action on the dropdown', async function (assert) {
-  //   assert.expect(1);
-  //   this.dropdown = {
-  //     uniqueId: 'e123',
-  //     isOpen: true,
-  //     actions: {
-  //       close() {
-  //         assert.ok(true, 'The close action gets called');
-  //       },
-  //       reposition() { }
-  //     }
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     <div id="other-div"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' isTouchDevice=true}}Lorem ipsum{{/basic-dropdown/content}}
-  //   `);
-
-  //   triggerEvent('#other-div', 'touchstart');
-  //   triggerEvent('#other-div', 'touchend');
-  // });
-
-  // test('Scrolling (touchstart + touchmove + touchend) anywhere in the app outside the component will invoke the close action on the dropdown', async function (assert) {
-  //   assert.expect(0);
-  //   this.dropdown = {
-  //     uniqueId: 'e123',
-  //     isOpen: true,
-  //     actions: {
-  //       close() {
-  //         assert.ok(false, 'The close action does not called');
-  //       },
-  //       reposition() { }
-  //     }
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     <div id="other-div"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' isTouchDevice=true}}Lorem ipsum{{/basic-dropdown/content}}
-  //   `);
-
-  //   triggerEvent('#other-div', 'touchstart');
-  //   triggerEvent('#other-div', 'touchmove');
-  //   triggerEvent('#other-div', 'touchend');
-  // });
-
-  // // Focus
-  // test('If it receives an `onFocusIn` action, it is invoked if a focusin event is fired inside the content', async function (assert) {
-  //   assert.expect(3);
-  //   this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
-  //   this.onFocusIn = (api, e) => {
-  //     assert.ok(true, 'The action is invoked');
-  //     assert.equal(api, this.dropdown, 'The first argument is the API');
-  //     assert.ok(e instanceof window.Event, 'the second argument is an event');
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' onFocusIn=onFocusIn}}
-  //       <input type="text" id="test-input-focusin" />
-  //     {{/basic-dropdown/content}}
-  //   `);
-  //   await focus('#test-input-focusin');
-  // });
-
-  // test('If it receives an `onFocusOut` action, it is invoked if a focusout event is fired inside the content', async function (assert) {
-  //   assert.expect(3);
-  //   this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
-  //   this.onFocusOut = (api, e) => {
-  //     assert.ok(true, 'The action is invoked');
-  //     assert.equal(api, this.dropdown, 'The first argument is the API');
-  //     assert.ok(e instanceof window.Event, 'the second argument is an event');
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' onFocusOut=onFocusOut}}
-  //       <input type="text" id="test-input-focusin" />
-  //     {{/basic-dropdown/content}}
-  //   `);
-  //   await focus('#test-input-focusin');
-  //   await blur('#test-input-focusin');
-  // });
-
-  // // Mouseenter/leave
-  // test('If it receives an `onMouseEnter` action, it is invoked if a mouseenter event is fired on the content', async function (assert) {
-  //   assert.expect(3);
-  //   this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
-  //   this.onMouseEnter = (api, e) => {
-  //     assert.ok(true, 'The action is invoked');
-  //     assert.equal(api, this.dropdown, 'The first argument is the API');
-  //     assert.ok(e instanceof window.Event, 'the second argument is an event');
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' onMouseEnter=onMouseEnter}}
-  //       Content
-  //     {{/basic-dropdown/content}}
-  //   `);
-  //   triggerEvent('.ember-basic-dropdown-content', 'mouseenter');
-  // });
-
-  // test('If it receives an `onMouseLeave` action, it is invoked if a mouseleave event is fired on the content', async function (assert) {
-  //   assert.expect(3);
-  //   this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
-  //   this.onMouseLeave = (api, e) => {
-  //     assert.ok(true, 'The action is invoked');
-  //     assert.equal(api, this.dropdown, 'The first argument is the API');
-  //     assert.ok(e instanceof window.Event, 'the second argument is an event');
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' onMouseLeave=onMouseLeave}}
-  //       Content
-  //     {{/basic-dropdown/content}}
-  //   `);
-  //   triggerEvent('.ember-basic-dropdown-content', 'mouseleave');
-  // });
-
-  // // Keydown
-  // test('If it receives an `onKeyDown` action, it is invoked if a keydown event is fired on the content', async function (assert) {
-  //   assert.expect(3);
-  //   this.dropdown = { uniqueId: 'e123', isOpen: true, actions: { reposition() { } } };
-  //   this.onKeyDown = (api, e) => {
-  //     assert.equal(api, this.dropdown, 'receives the dropdown as 1st argument');
-  //     assert.ok(e instanceof window.Event, 'It receives the event as second argument');
-  //     assert.equal(e.keyCode, 70, 'the event is the keydown event');
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el' onKeyDown=onKeyDown}}
-  //       <input id="inner-input">
-  //     {{/basic-dropdown/content}}
-  //   `);
-  //   await focus('#inner-input');
-  //   await triggerKeyEvent('#inner-input', 'keydown', 70);
-  // });
-
-  // // Repositining
-  // test('The component is repositioned immediatly when opened', async function (assert) {
-  //   assert.expect(1);
-  //   this.dropdown = {
-  //     uniqueId: 'e123',
-  //     isOpen: true,
-  //     actions: {
-  //       reposition() {
-  //         assert.ok(true, 'Reposition is invoked exactly once');
-  //       }
-  //     }
-  //   };
-  //   await render(hbs`
-  //     <div id="destination-el"></div>
-  //     {{#basic-dropdown/content dropdown=dropdown destination='destination-el'}}Lorem ipsum{{/basic-dropdown/content}}
-  //   `);
-  // });
+  // Repositining
+  test('The component is repositioned immediately when opened', async function (assert) {
+    assert.expect(1);
+    this.dropdown = {
+      uniqueId: 'e123',
+      isOpen: true,
+      actions: {
+        reposition() {
+          assert.ok(true, 'Reposition is invoked exactly once');
+        }
+      }
+    };
+    await render(hbs`
+      <div id="destination-el"></div>
+      <BasicDropdownContent @id="my-content" @dropdown={{this.dropdown}} @destination="destination-el">Lorem ipsum</BasicDropdownContent>
+    `);
+  });
 
   // test('The component is not repositioned if it is closed', async function (assert) {
   //   assert.expect(0);
